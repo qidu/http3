b71a49936c2c5ccc547c5492eab3037d5072d8aa
sudu <teric@outlook.com>
2023-10-10 15:10:25 +0800

    Update raw_pub_key.sh

--- raw_pub_key.sh
+++ raw_pub_key.sh
@@ -5,3 +5,4 @@ openssl ec -in private.key -pubout -out public.key
 openssl ec -in public.key -pubout -outform DER -out raw_public.key
 #openssl ec -in public.key -pubout -outform DER -out raw_public.key
 openssl ec -in private.key -pubout -outform PEM -out keypair.pem
+openssl x509 -req -in csr.pem -CA root.crt -CAkey root.key -CAcreateserial -out cert.pem -days 365
