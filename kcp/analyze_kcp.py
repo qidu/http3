53610de275421b27d24c0b426c444b5309d060a4
sudu <teric@outlook.com>
2023-10-31 15:45:43 +0800

    Update analyze_kcp.py

--- kcp/analyze_kcp.py
+++ kcp/analyze_kcp.py
@@ -7,18 +7,26 @@ url = ""
 tm = ""
 res = {}
 
-with open('result-kcp3.log') as f:
+with open('result-kcp-suf1.log') as f:
+    readurl = False
+    readtime = False
     for line in f:
         part = line.split(' ')
-
         if len(part) == 4:
             url = part[3]
             url = url.replace("\n", "").replace("\r", "").replace(" ", "").replace("http:", "").replace("/", "").replace("kcptunclient", "")
+            readurl = True
         part = line.split('=')
         if len(part) == 2:
             tm = part[1]
             tm = tm.replace("\n", "").replace("\r", "").replace(" ", "").replace("s", "")
+            readtime = True
         if idx % 2 == 1:
+            if not (readurl and readtime): # skip 2 lines
+                url = ""
+                tm = 0
+                idx += 1
+                continue
             #print(url, tm)
             if url not in res:
                 res[url] = []
@@ -29,9 +37,8 @@ for key, values in res.items():
     plt.plot(values, label=key)
 
 plt.legend()
-plt.title('TCP vs KCP')
+plt.title('TCP vs KCP @Suf')
 plt.xlabel('Tests')
 plt.ylabel('Time(s)')
-
 #plt.show()
-plt.savefig('./testkcp.png')
+plt.savefig('./testkcp_suf.png')
