58969bdf7c5b6c8e487005827392b2a56229b31a
qidu <teric@outlook.com>
2023-11-01 10:23:29 +0800

    update send and recv demo

--- kcp/kcprecv.cpp
+++ kcp/kcprecv.cpp
@@ -12,6 +12,7 @@
 #define BUFFER_SIZE 200000
 #define MAX_PACKET_SIZE 1500
 #define FILENAME "/tmp/filename2"
+#define DUMP_TO_FILE
 
 /* get system time */
 static inline void itimeofday(long *sec, long *usec)
@@ -60,50 +61,22 @@ typedef struct {
     int sockfd;
     struct sockaddr_in addr;
     ikcpcb *kcp;
-    struct event_base *base;
     int total;
     int net_total;
-	IUINT32 current;
-	IUINT32 nextupdate;
+    FILE *file;
 } Handle;
 
 Handle handle;
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
-// KCP回调函数，用于处理接收到的数据
+// KCP回调函数
 int kcp_output(const char *buf, int len, ikcpcb *kcp, void *user) {
     Handle *handle = (Handle *)user;
     int n = sendto(handle->sockfd, buf, len, 0, (struct sockaddr*)&handle->addr, sizeof(handle->addr));
-    // fprintf(stderr, "> kcp output callback %d send %d\n", len, n);
     return 0;
 }
 
-void timer_callback(evutil_socket_t fd, short events, void *arg) {
-    Handle *handle = (Handle *)arg;
-
-    pthread_mutex_lock(&lock);
-	
-	IUINT32 current = iclock();
-	if (current >= handle->nextupdate) {
-    	ikcp_update(handle->kcp, current);
-		handle->nextupdate = ikcp_check(handle->kcp, current);
-	}
-
-    int peeksize = 0;
-    while(peeksize = ikcp_peeksize(handle->kcp), peeksize > 0) {
-        // 从KCP中接收数据
-        char recvBuffer[BUFFER_SIZE];
-        int recvlen = 0;
-	while(recvlen = ikcp_recv(handle->kcp, recvBuffer, BUFFER_SIZE), recvlen > 0) {
-            handle->total += recvlen;
-            fprintf(stderr, "kcp recv %d peak %d total kcp %d net %d\r", recvlen, peeksize, handle->total, handle->net_total);
-	}
-    }
-    pthread_mutex_unlock(&lock);
-}
-
-
-void *receive_handler(void *arg) {
+void do_receive(void *arg) {
     Handle *handle = (Handle *)arg;
     int sockfd = handle->sockfd;
     char buffer[MAX_PACKET_SIZE];
@@ -115,82 +88,93 @@ void *receive_handler(void *arg) {
     addr.sin_addr.s_addr = INADDR_ANY;
     while (1) {
         int n = recvfrom(sockfd, buffer, MAX_PACKET_SIZE, 0, (struct sockaddr *)&addr, &len);
-        if (n < 0) {
-            perror("recvfrom");
-            exit(EXIT_FAILURE);
-        } else if (n == 0) {
-            printf("Peer closed the connection\n");
-            break;
+        if (n <= 0) {
+            usleep(10000);
+            continue;
         } else {
-			pthread_mutex_lock(&lock);
 			handle->net_total += n;
 			memcpy(&handle->addr, &addr, sizeof(addr));
         	int input = ikcp_input(handle->kcp, buffer, n);
-			pthread_mutex_unlock(&lock);
-
-			// fprintf(stderr, "< recving %d input %d\n", n, input);
+            handle->kcp->current = iclock();
+	        ikcp_flush(handle->kcp);
+            
+            int peeksize = 0;
+            if (peeksize = ikcp_peeksize(handle->kcp), peeksize > 0) {
+                // 从KCP中接收数据
+                char recvBuffer[BUFFER_SIZE];
+                int recvlen = 0;
+                while(recvlen = ikcp_recv(handle->kcp, recvBuffer, BUFFER_SIZE), recvlen > 0) {
+                    handle->total += recvlen;
+                    if (recvlen >= 3 && strncmp(&recvBuffer[recvlen - 3], "bye", 3) == 0) {
+                        fprintf(stderr, "kcp recv %d peak %d total kcp %d net %d    \n", recvlen, peeksize, handle->total, handle->net_total);
+                        fprintf(stderr, "bye.\n");
+                        return;
+                    }
+                    else {
+#ifdef DUMP_TO_FILE
+                        fwrite(recvBuffer, 1, recvlen, handle->file);
+#endif
+                        fprintf(stderr, "kcp recv %d peak %d total kcp %d net %d ...\r", recvlen, peeksize, handle->total, handle->net_total);
+                    }
+                }
+            }
         }
     }
-    close(sockfd);
-    return NULL;
 }
 
-int main() {
-    // 创建文件
+int set_dscp(int sock, int iptos) {
+    iptos = (iptos << 2) & 0xFF;
+    return setsockopt(sock, IPPROTO_IP, IP_TOS, &iptos, sizeof(iptos));
+}
+
+int main(int argc, char* argv[]) 
+{
+#ifdef DUMP_TO_FILE
     FILE *file = fopen(FILENAME, "wb");
     if (!file) {
         perror("Failed to open file");
         return 1;
     }
-
+    handle.file = file;
+#endif
     // 创建UDP套接字
     int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    set_dscp(sockfd, 46);
 	handle.sockfd = sockfd;
-    // evutil_make_socket_nonblocking(sockfd);
+    evutil_make_socket_nonblocking(sockfd);
     
     struct sockaddr_in addr;
     socklen_t len = sizeof(addr);
     memset(&addr, 0, sizeof(addr));
     addr.sin_family = AF_INET;
-    addr.sin_port = htons(12345);
+    if (argc > 1) {
+        addr.sin_port = htons(atoi(argv[1]));
+    }
+    else {
+        addr.sin_port = htons(12345);
+    }
     addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    // 直接使用bind系统调用绑定
-    int ret = bind(sockfd, (struct sockaddr*)&addr, sizeof(addr));
-    fprintf(stderr, "bind %d\n", ret);
 
-    // 创建libevent上下文
-    struct event_base *base = event_base_new();
-    handle.base = base;
-    
-    struct event *timer_event = event_new(base, -1, EV_PERSIST, timer_callback, &handle);
-    struct timeval timer_interval = {0, 10000};
-    event_add(timer_event, &timer_interval);
+    int ret = bind(sockfd, (struct sockaddr*)&addr, sizeof(addr));
+    fprintf(stderr, "bind %s.\n", ret == 0 ? "success" : "failed");
 
     // 创建KCP实例
-    handle.kcp = ikcp_create(0x11223344, (void *)&handle);
+    handle.kcp = ikcp_create(0x11223344, 0, (void *)&handle);
     handle.kcp->output = kcp_output;
     handle.kcp->stream = 1;
-    handle.nextupdate = iclock();
 
     // 设置KCP参数
-    ikcp_wndsize(handle.kcp, 32, 32);
+    ikcp_wndsize(handle.kcp, 2048, 2048);
     ikcp_nodelay(handle.kcp, 0, 40, 0, 0);
     //ikcp_nodelay(handle.kcp, 1, 10, 2, 1);
-    ikcp_setmtu(handle.kcp, 1200);
+    ikcp_setmtu(handle.kcp, 1400);
 
-    pthread_t thread;
-    ret = pthread_create(&thread, NULL, receive_handler, &handle);
+    do_receive(&handle);
 
-    event_base_dispatch(base);
-
-    pthread_join(thread, NULL); 
-
-    // 关闭KCP和libevent
-    // ikcp_release(kcp);
-    event_base_free(base);
-
-    // 关闭文件和套接字
+    ikcp_release(handle.kcp);
+#ifdef DUMP_TO_FILE    
     fclose(file);
+#endif
     close(sockfd);
 
     return 0;
--- kcp/kcpsend.cpp
+++ kcp/kcpsend.cpp
@@ -14,8 +14,8 @@
 
 #include "ikcp.h"
 
-#define BUFFER_SIZE 20000 
-#define PACKET_SIZE 1000
+#define BUFFER_SIZE 100000
+#define PACKET_SIZE 1400
 
 #define FILENAME "/tmp/filename"
 #define TARGET_IP "127.0.0.1"
@@ -70,12 +70,9 @@ typedef struct {
     int sended;
     int inbuffer;
     ikcpcb *kcp;
+    struct sockaddr_in server;
     struct event_base *base;
-    struct bufferevent* bev;
-    struct evbuffer* inputbuf;
-    struct evbuffer* outputbuf;
     int total;
-    IUINT32 nextupdate;
 } Handle; 
 
 Handle handle;
@@ -84,90 +81,67 @@ Handle handle;
 int kcp_output(const char *buf, int len, ikcpcb *kcp, void *user) 
 {
     Handle *handle = (Handle *)user;
-    bufferevent_write(handle->bev, buf, len);
-    bufferevent_flush(handle->bev, EV_WRITE, BEV_FLUSH);
-    size_t buffer_len = evbuffer_get_length(bufferevent_get_output(handle->bev));
-//    fprintf(stderr, "> kcp output %d buffering %lu\n", len, buffer_len);
-	handle->inbuffer -= len;
+    handle->inbuffer -= len;  // total len bigger than inbuffer
+
+    int sent = 0;
+    int packet_len = PACKET_SIZE;
+    char buffer[PACKET_SIZE];
+    while (len > 0) {
+        packet_len = PACKET_SIZE;
+        if (len < PACKET_SIZE) {
+            packet_len = len;
+        }
+        memcpy(buffer, buf + sent, packet_len);
+        sent += packet_len;
+        sendto(handle->sockfd, buffer, packet_len, 0, (struct sockaddr *)&handle->server, sizeof(struct sockaddr_in));
+        len -= packet_len;
+    }
+
     return 0;
-}
-
-void connect_callback(struct bufferevent* bev, short events, void* ctx)
-{
-    Handle *handle = (Handle *)ctx;
-    if (events & BEV_EVENT_CONNECTED)
-    {
-        printf("Connected .\n");
-    }
-    if (events & BEV_EVENT_ERROR)
-    {
-        printf("Error.\n");
-    }
-    if (events & BEV_EVENT_READING)
-    {
-        printf("READING.\n");
-    }
-    if (events & BEV_EVENT_WRITING)
-    {
-        printf("WRITING.\n");
-    }
-    if (events & BEV_EVENT_EOF)
-    {
-        printf("Eof.\n");
-    }
-    else
-    {
-        printf("Connection failed %d.\n", events);
-        // event_base_loopbreak(bufferevent_get_base(bev));
-    }
-}
-
-void read_callback(struct bufferevent* bev, void* ctx)
-{
-    Handle *handle = (Handle *)ctx;
-
-    // 读取数据
-    char buf[PACKET_SIZE];
-    size_t len = 0;
-    while ( len = bufferevent_read(handle->bev, buf, PACKET_SIZE), len > 0)
-    {
-        int input = ikcp_input(handle->kcp, buf, len);
-    //    fprintf(stderr, "< net recv %ld kcp input %d\n", len, input);
-    }
-	// perror("reading");
-}
-
-// 写事件回调函数
-void write_callback(struct bufferevent* bev, void* ctx) {
-    // 从KCP发送数据
-    Handle *handle = (Handle *)ctx;
-	// perror("writing");
 }
 
 // 定时器回调函数，用于触发KCP更新
 void timer_callback(evutil_socket_t fd, short events, void *arg)
 {
     Handle *handle = (Handle *)arg;
-	IUINT32 current = iclock();
-	if (current >= handle->nextupdate) {
-    	ikcp_update(handle->kcp, current);
-		handle->nextupdate = ikcp_check(handle->kcp, current);
-	}
-
 	if (handle->inbuffer < 0) {
     	char buffer[BUFFER_SIZE];
     	handle->inbuffer = fread(buffer, 1, BUFFER_SIZE, handle->file);
 		if (handle->inbuffer > 0) {
         	handle->total += handle->inbuffer;
-        	fprintf(stderr, "@ read %d total %d\r", handle->inbuffer, handle->total);
+        	fprintf(stderr, "@ read %d total %d ...\r", handle->inbuffer, handle->total);
         	ikcp_send(handle->kcp, buffer, handle->inbuffer);
-        	ikcp_flush(handle->kcp);
 		}
 		else {
-			fprintf(stderr, "read file error, quiting\n");
-    		event_base_loopbreak(bufferevent_get_base(handle->bev));
+			fprintf(stderr, "@ read total %d\n", handle->total);
+    		event_base_loopbreak(handle->base);
 		}
 	}
+    handle->kcp->current = iclock();
+    ikcp_flush(handle->kcp);
+}
+
+void read_callback(evutil_socket_t sockfd, short event, void *arg) 
+{
+    char buffer[BUFFER_SIZE];
+    Handle *handle = (Handle *)arg;
+    if (event & EV_READ) {
+        socklen_t addr_len;
+        ssize_t ret = recvfrom(handle->sockfd, buffer, sizeof(buffer), 1, (struct sockaddr *)&handle->server, &addr_len);
+        if (ret > 0) {
+            int input = ikcp_input(handle->kcp, buffer, ret);
+        }
+        handle->kcp->current = iclock();
+        ikcp_flush(handle->kcp);
+    }
+    else {
+
+    }
+}
+
+int set_dscp(int sock, int iptos) {
+    iptos = (iptos << 2) & 0xFF;
+    return setsockopt(sock, IPPROTO_IP, IP_TOS, &iptos, sizeof(iptos));
 }
 
 int main(int argc, char* argv[]) 
@@ -175,74 +149,65 @@ int main(int argc, char* argv[])
     // 打开文件
     handle.file = fopen(FILENAME, "rb");
     if (!handle.file) {
-        perror("Failed to open file");
+        fprintf(stderr, "Failed to open file %s", FILENAME);
         return 1;
     }
     
     
     // 创建UDP套接字
     handle.sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    set_dscp(handle.sockfd, 46);
     evutil_make_socket_nonblocking(handle.sockfd);
     // 创建libevent上下文
     handle.base = event_base_new();
-    handle.bev = bufferevent_socket_new(handle.base, handle.sockfd, BEV_OPT_CLOSE_ON_FREE);
-
-    // 创建写事件
-    bufferevent_setcb(handle.bev, read_callback, write_callback, NULL, &handle);
-    bufferevent_setwatermark(handle.bev, EV_WRITE, 0, 10);
 
     struct sockaddr_in serv_addr;
     memset(&serv_addr, 0, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(TARGET_PORT);
-    inet_pton(AF_INET, (argc > 1? argv[1] : TARGET_IP), &(serv_addr.sin_addr));
+    if (argc > 1) {
+        inet_pton(AF_INET, argv[1], &(serv_addr.sin_addr));
+    }
+    else {
+        inet_pton(AF_INET, TARGET_IP, &(serv_addr.sin_addr));
+    }
+    memcpy(&handle.server, &serv_addr, sizeof(struct sockaddr_in));
 
-    bufferevent_socket_connect(handle.bev, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
-    bufferevent_enable(handle.bev, EV_READ | EV_WRITE);
+    struct event *ev = event_new(handle.base, handle.sockfd, EV_READ | EV_PERSIST, read_callback, &handle);
+    event_add(ev, NULL);
 
-    // 创建定时器事件，每隔10毫秒触发一次
     struct event *timer_event = event_new(handle.base, -1, EV_PERSIST, timer_callback, &handle);
-    struct timeval timer_interval = {0, 50000};
+    struct timeval timer_interval = {0, 10000};
     event_add(timer_event, &timer_interval);
 	
-	// 创建KCP实例
-    handle.kcp = ikcp_create(0x11223344, (void*)&handle);
+    handle.kcp = ikcp_create(0x11223344, 0, (void*)&handle);
     handle.kcp->output = kcp_output;
     handle.kcp->stream = 1;
-    handle.nextupdate = iclock();
 
-    // 设置KCP参数
-    ikcp_wndsize(handle.kcp, 32, 32);
+    ikcp_wndsize(handle.kcp, 512, 512);
     ikcp_nodelay(handle.kcp, 0, 40, 0, 0);
     //ikcp_nodelay(handle.kcp, 1, 10, 2, 1);
-    ikcp_setmtu(handle.kcp, 1200);
+    ikcp_setmtu(handle.kcp, 1400);
 
-    // 读取文件数据
-    char buffer[BUFFER_SIZE];
-    if ((handle.inbuffer = fread(buffer, 1, BUFFER_SIZE, handle.file)) > 0) {
-        handle.total += handle.inbuffer;
-        fprintf(stderr, "@ read %d total %d\n", handle.inbuffer, handle.total);
-        ikcp_send(handle.kcp, buffer, handle.inbuffer);
-        ikcp_flush(handle.kcp);
-    }
+    handle.inbuffer = -1;
   
     event_base_dispatch(handle.base);
+    ikcp_send(handle.kcp, "bye", 3);
 
     // 发送结束标识
-    fprintf(stderr, "finish send, waisnd\n");
+    fprintf(stderr, "finish reading file, and wait sending.\n");
 
     // 进入事件循环，直到所有数据发送完成
     int ret = 0;
-    while (ret = ikcp_waitsnd(handle.kcp) > 0) {
-        // fprintf(stderr, "waitsnd finish... %d\n", ret);
+    while ((ret = ikcp_waitsnd(handle.kcp)) > 0) {
         event_base_dispatch(handle.base);
-		sleep(1);
     }
+    fprintf(stderr, "finish sending.\n");
 
+    
     // 释放资源
     ikcp_release(handle.kcp);
     event_base_free(handle.base);
-    event_free(timer_event);
 
     // 关闭文件和套接字
     fclose(handle.file);
