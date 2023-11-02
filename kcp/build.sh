c20fc0a5259b7106b815996e7b1419c3d8a9af51
qidu <teric@outlook.com>
2023-11-02 17:55:46 +0800

    fix build aarch64 for arm android

--- kcp/build.sh
+++ kcp/build.sh
@@ -1,9 +1,11 @@
 # apt install gcc-arm-linux-gnueabi
 # apt install g++-arm-linux-gnueabi
+# apt install gcc-aarch64-linux-gnu
 gcc -g -I./kcp2 kcpsend.cpp kcp2/ikcp.c -levent -o send
 gcc -g -I./kcp2 kcprecv.cpp kcp2/ikcp.c -levent -o recv
-if [ -z /usr/bin/arm-linux-gnueabi-gcc ]
+if [ -f /usr/bin/aarch64-linux-gnu-gcc ]
 then
-    /usr/bin/arm-linux-gnueabi-g++ -I./kcp2 kcprecv.cpp kcp2/ikcp.c -levent -o recv_kcparm
-    /usr/bin/arm-linux-gnueabi-g++ -I./kcp2 kcpsend.cpp kcp2/ikcp.c -levent -o send_kcparm
+# sudo -E ./configure --host=arm-linux-gnueabi --build=arm64 CC=/usr/bin/aarch64-linux-gnu-gcc
+    /usr/bin/aarch64-linux-gnu-gcc -I./kcp2 kcprecv.c kcp2/ikcp.c -levent -o recv_kcparm
+    /usr/bin/aarch64-linux-gnu-gcc -I./kcp2 kcpsend.c kcp2/ikcp.c -levent -o send_kcparm
 fi
